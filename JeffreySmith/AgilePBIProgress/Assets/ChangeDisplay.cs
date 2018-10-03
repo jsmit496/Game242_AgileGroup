fd5dafadfa464f0bfc49585d1c6e7c263c4c45f1
svc-student <svc-student@student.directory.jccc>
2018-10-03 13:47:04 -0500

    work on sprint 2

---
+++ JeffreySmith/AgilePBIProgress/Assets/ChangeDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDisplay : MonoBehaviour
+{
+    public GameObject[] ChangeDisplayObjects;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (gameObject.activeSelf)
+            {
+                gameObject.SetActive(false);
+            }
+            else if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+	}
+}
