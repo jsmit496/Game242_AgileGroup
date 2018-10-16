87d0a2fc18bda640242dbf938df43851d8b1780f
Nathan Yang <yangnathan0325>
2018-10-16 15:52:17 -0500

    First update
    update

---
+++ NathanYang/Game 242 Sprint 2/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour {
+
+    // Choosing of the cursor & texture
+    [System.Serializable]
+    public class TheCursor
+    {
+        public string tag;
+        public Texture2D cursorTexture;
+    }
+
+    public List<TheCursor> cursorList = new List<TheCursor>();
+
+	void Start () {
+        SetCursorTexture(cursorList[0].cursorTexture);
+	}
+	
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            for (int i = 0; i < cursorList.Count; i++)
+            {
+                if (hit.collider.tag == cursorList[i].tag)
+                {
+                    SetCursorTexture(cursorList[i].cursorTexture);
+                    return;
+                }
+            }
+        }
+        SetCursorTexture(cursorList[0].cursorTexture);
+	}
+
+    void SetCursorTexture(Texture2D tex)
+    {
+        Cursor.SetCursor(tex, Vector2.zero, CursorMode.Auto);
+    }
+}
---
+++ NathanYang/Game 242 Sprint 2/Assets/Scripts/TestScripts/SwitchScript1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchScript1 : MonoBehaviour {
+
+    // For Text UI
+    public GameObject SwitchText;
+
+    // Given Object
+    public GameObject designatedObject;
+
+    // True/False Switch being pressed
+    public bool switchPressed = false;
+
+    // True/False if player is present near the object
+    public bool switchPresent = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Switch")
+        {
+            SwitchText.SetActive(true);
+            SwitchText.GetComponent<Text>().text = "Left Click to activate Switch";
+            switchPresent = true;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Switch")
+        {
+            switchPresent = true;
+            if (Input.GetKeyDown(KeyCode.Mouse0) && switchPresent == true)
+            {
+                switchPressed = true;
+                Debug.Log("Switch Pressed");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        SwitchText.SetActive(false);
+        SwitchText.GetComponent<Text>().text = "";
+        switchPresent = false;
+    }
+
+    public void Update()
+    {
+        if (switchPressed == true)
+        {
+            designatedObject.transform.Translate(Vector3.down * Time.deltaTime);
+            switchPressed = true;
+        }
+    }
+}
