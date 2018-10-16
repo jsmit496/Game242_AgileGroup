3de023a5f13ef053e1d873ceb18a9fbcef2e5a62
DESKTOP-HL219CL\Nicolas Rovriosa <nroviros@stumail.jccc.edu>
2018-10-16 12:50:17 -0500

    Character Controller V1.0
    Added Character Controller V1.0

---
+++ NicolasRovirosa/Character Controller/Assets/Scripts/ClimbEntrance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbEntrance : MonoBehaviour {
+
+    private Transform Waypoint;
+
+    private void Start()
+    {
+        Waypoint = transform.parent.Find("EntranceWaypoint");
+    }
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+
+    }
+
+    private void OnTriggerStay(Collider Player)
+    {
+        if (GameObject.Find("Player").GetComponent<PlayerController>().transferClimb == false)
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().wayPoint = Waypoint;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().transferClimb = true;
+            GameObject.Find("Player").GetComponent<PlayerController>().allowFall = false;
+            GameObject.Find("Player").GetComponent<PlayerController>().GotoWaypoint();
+        }
+    }
+}
