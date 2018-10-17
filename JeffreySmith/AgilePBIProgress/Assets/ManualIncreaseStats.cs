d083b1469b9f04d8d70366788a267f6be9c59d3c
jsmit496 <jsmit496@stumail.jccc.edu>
2018-10-17 08:07:29 -0500

    Added manual conttrol of some refill stuff

---
+++ JeffreySmith/AgilePBIProgress/Assets/ManualIncreaseStats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualIncreaseStats : MonoBehaviour
+{
+
+
+    private ManagePlayerStats playerStats;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<ManagePlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerStats.currentHealth += 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerStats.currentHealth -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerStats.currentFatigue += 1;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerStats.currentStamina -= 1;
+        }
+	}
+}
